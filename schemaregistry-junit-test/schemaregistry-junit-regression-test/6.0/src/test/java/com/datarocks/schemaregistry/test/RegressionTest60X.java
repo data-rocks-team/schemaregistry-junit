8aa216a245da37688c23e52c0238141197b52c33
fnobilia <f.nobilia@gmail.com>
2021-02-20 19:44:35 +0000

    Bump confluent to 6.1.0

---
+++ schemaregistry-junit-test/schemaregistry-junit-regression-test/6.0/src/test/java/com/datarocks/schemaregistry/test/RegressionTest60X.java
@@ -0,0 +1,9 @@
+package com.datarocks.schemaregistry.test;
+
+public class RegressionTest60X extends RegressionTest {
+
+  @Override
+  public String topic() {
+    return "regression-test-6.0.x";
+  }
+}
