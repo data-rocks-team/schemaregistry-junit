f6dbb78375e32eb94e31cf730137ca87dd488f8f
fnobilia <f.nobilia@gmail.com>
2021-11-27 17:12:55 +0000

    Bump confluentVersion from 6.2.1 to 7.0.0

---
+++ regression-test/6.2/src/test/java/com/datarocks/schemaregistry/test/RegressionTest62X.java
@@ -0,0 +1,9 @@
+package com.datarocks.schemaregistry.test;
+
+public class RegressionTest62X extends RegressionTest {
+
+  @Override
+  public String topic() {
+    return "regression-test-6.2.x";
+  }
+}
