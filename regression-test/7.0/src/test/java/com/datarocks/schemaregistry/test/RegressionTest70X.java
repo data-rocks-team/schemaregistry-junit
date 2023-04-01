e2ba10999302f3013b51501e5cbc8e6713838de9
Francesco Nobilia <f.nobilia@gmail.com>
2023-04-01 23:23:40 +0100

    Bump Confluent to 7 (#152)

---
+++ regression-test/7.0/src/test/java/com/datarocks/schemaregistry/test/RegressionTest70X.java
@@ -0,0 +1,9 @@
+package com.datarocks.schemaregistry.test;
+
+public class RegressionTest70X extends RegressionTest {
+
+  @Override
+  public String topic() {
+    return "regression-test-7.0.x";
+  }
+}
---
+++ regression-test/7.1/src/test/java/com/datarocks/schemaregistry/test/RegressionTest71X.java
@@ -0,0 +1,9 @@
+package com.datarocks.schemaregistry.test;
+
+public class RegressionTest71X extends RegressionTest {
+
+  @Override
+  public String topic() {
+    return "regression-test-7.1.x";
+  }
+}
